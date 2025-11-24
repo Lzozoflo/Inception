17207e10d3001c5c1766cc875431f3443f2bb85b
fcretin <florent.cretin@hotmail.fr>
2025-11-24 15:19:36 +0100

    maybe

--- srcs/requirements/mariadb/conf/mariadb_entrypoint.sh
+++ srcs/requirements/mariadb/conf/mariadb_entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-
 echo "[INFO] Initialisation de MariaDB..."
-
 
 # Initialiser la base de données si elle n'existe pas
 if [ ! -d "/var/lib/mysql/mysql" ]; then
@@ -13,21 +11,22 @@ else
     echo "[INFO] Installation système MariaDB deja faite..."
 fi
 
-
 # Var d'env attendues :
 #   MYSQL_DATABASE, MYSQL_ADMYN_USER, MYSQL_ADMYN_PASSWORD, MYSQL_NOT_ADMYN_USER, MYSQL_NOT_ADMYN_PASSWORD
 
-
 # Si la base WordPress n'existe pas, la créer
-if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+if [ ! -d "/var/lib/mysql/init.sql" ]; then
     echo "[INFO] Création de la base '$MYSQL_DATABASE' et des utilisateurs..."
 
     cat > /var/lib/mysql/init.sql << EOF
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db LIKE 'test_%';
 CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;
 CREATE USER IF NOT EXISTS '$MYSQL_ADMYN_USER'@'%' IDENTIFIED BY '$MYSQL_ADMYN_PASSWORD';
 CREATE USER IF NOT EXISTS '$MYSQL_NOT_ADMYN_USER'@'%' IDENTIFIED BY '$MYSQL_NOT_ADMYN_PASSWORD';
-GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_ADMYN_USER'@'%';
-GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_NOT_ADMYN_USER'@'%';
+GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO  '$MYSQL_ADMYN_USER'@'%' IDENTIFIED BY '$MYSQL_ADMYN_PASSWORD';
+GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_NOT_ADMYN_USER'@'%' IDENTIFIED BY '$MYSQL_NOT_ADMYN_PASSWORD';
 FLUSH PRIVILEGES;
 EOF
 
--- srcs/requirements/wordpress/conf/wordpress_entrypoint.sh
+++ srcs/requirements/wordpress/conf/wordpress_entrypoint.sh
@@ -21,22 +21,24 @@ fi
 if [ ! -f "wp-config.php" ]; then
     echo "[INFO] Create Config..."
     wp config create \
-    --dbname="${WP_DB_NAME:-WordPress}" \
-    --dbuser="${WP_DB_USER:-admin}" \
-    --dbpass="${WP_DB_PASS:-adminadmin}" \
-    --dbhost="${WP_DB_HOST:-db}" \
+    --dbname="${WP_DB_NAME}" \
+    --dbuser="${WP_DB_USER}" \
+    --dbpass="${WP_DB_PASS}" \
+    --dbhost="${WP_DB_HOST}" \
     --skip-check \
     --allow-root
 
     echo "[INFO] Install WordPress..."
     wp core install \
-    --url="${WP_URL:-http://localhost}" \
+    --url="${WP_URL}" \
     --title="${WP_TITLE:-Mon site WordPress}" \
-    --admin_user="${WP_ADMIN_USER:-admin}" \
-    --admin_password="${WP_ADMIN_PASS:-admin}" \
-    --admin_email="${WP_ADMIN_EMAIL:-admin@example.com}" \
+    --admin_user="${WP_ADMIN_USER}" \
+    --admin_password="${WP_ADMIN_PASS}" \
+    --admin_email="${WP_ADMIN_EMAIL:-admyn@example.com}" \
     --skip-email \
     --allow-root
+
+
 
     echo "[INFO] Finish Install WordPress..."
 else
